6212b7c86e796833e777cd1c1c5b5c0db1da85a4
AvramAlexandraIoana <37377341+AvramAlexandraIoana@users.noreply.github.com>
2021-04-20 19:59:17 +0100

    Added preautorize for admin and user

--- src/main/java/com/awbd/proiect/controllers/CountryController.java
+++ src/main/java/com/awbd/proiect/controllers/CountryController.java
@@ -9,6 +9,7 @@ import com.awbd.proiect.services.CountryServiceImpl;
 import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -17,7 +18,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/country")
+@RequestMapping("/api/country")
 public class CountryController {
     private CountryService countryService;
 
@@ -26,6 +27,7 @@ public class CountryController {
     }
 
     @GetMapping("/list")
+    @PreAuthorize("hasRole('ROLE_USER')  or hasRole('ROLE_ADMIN')")
     public
     ResponseEntity<List<Country>> get() {
         return ResponseEntity.status(HttpStatus.ACCEPTED)
@@ -33,6 +35,7 @@ public class CountryController {
     }
 
     @PostMapping()
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public
     ResponseEntity<Country> save(@RequestBody Country country) {
         Country savedCountry = countryService.save(country);
@@ -46,19 +49,23 @@ public class CountryController {
 
     }
 
+
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Country> getById(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(countryService.findById(id));
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Country> update(@RequestBody Country country) {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(countryService.update(country));
     }
 
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void deleteById(@PathVariable Long id) {
         countryService.deleteById(id);
     }
